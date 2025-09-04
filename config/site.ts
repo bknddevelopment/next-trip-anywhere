b1ad6c5114f7b9897aafd31d5a04a52b5d162e9c
Charwin van Ryck de Groot <charwin@nexttripanywhere.com>
2025-09-04 17:56:58 -0400

    Remove debug output from site config

--- config/site.ts
+++ config/site.ts
@@ -5,12 +5,6 @@
 const isProd = process.env.NODE_ENV === 'production'
 export const BASE_PATH = isProd ? '/next-trip-anywhere' : ''
 
-// Debug output during build
-if (typeof window === 'undefined') {
-  console.log('[Build] NODE_ENV:', process.env.NODE_ENV)
-  console.log('[Build] BASE_PATH:', BASE_PATH)
-}
-
 export const siteConfig = {
   basePath: BASE_PATH,
   logoPath: `${BASE_PATH}/NextTripAnywhere.PNG`,
